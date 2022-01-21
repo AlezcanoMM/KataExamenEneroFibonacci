3803e21bb1100c0f2481c4f1eb8ac188c31b8e5c
Alezcanomm <Alezcanomm@gmail.com>
2022-01-21 10:06:06 +0000

    Minimum code added for test 3, green

--- src/main/java/es/ulpgc/hpds/Fibonacci.java
+++ src/main/java/es/ulpgc/hpds/Fibonacci.java
@@ -3,6 +3,9 @@ package es.ulpgc.hpds;
 public class Fibonacci {
 
     public int getFibonacciOf(int n) {
-        return n;
+        if(n == 0 || n == 1){
+            return n;
+        }
+        return 1;
     }
 }
