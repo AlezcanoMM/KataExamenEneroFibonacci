6199f20f07258780ec0b2fb029c5e47af9f5a794
Alezcanomm <Alezcanomm@gmail.com>
2022-01-21 10:41:52 +0000

    ProductFibonacci with test 800 passed, green

--- src/main/java/es/ulpgc/hpds/ProductFibonacci.java
+++ src/main/java/es/ulpgc/hpds/ProductFibonacci.java
@@ -13,6 +13,11 @@ public class ProductFibonacci {
                 res[1] = fibo.getFibonacciOf(n+1);
                 res[2] = 1;
                 return res;
+            } else if((fibo.getFibonacciOf(n) * fibo.getFibonacciOf(n+1)) > product){
+                res[0] = fibo.getFibonacciOf(n);
+                res[1] = fibo.getFibonacciOf(n+1);
+                res[2] = 0;
+                return res;
             }
             n++;
         }
