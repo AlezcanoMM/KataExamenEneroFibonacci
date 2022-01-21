f31e26c468f5526c81d56d961c693eec93528941
Alezcanomm <Alezcanomm@gmail.com>
2022-01-21 10:35:40 +0000

    ProductFibonnacci testing with 714 failed, red

--- src/test/java/es/ulpgc/hpds/ProductFibonacciTest.java
+++ src/test/java/es/ulpgc/hpds/ProductFibonacciTest.java
@@ -14,7 +14,7 @@ public class ProductFibonacciTest {
     public void setUp(){
         productFibo = new ProductFibonacci();
     }
-    
+
     @Test
     public void test_Product_Of_Fibonacci_0(){
         expectedResult = new int[]{0, 1, 1};
@@ -25,5 +25,11 @@ public class ProductFibonacciTest {
     public void test_Product_Of_Fibonacci_1(){
         expectedResult = new int[]{1, 2, 1};
         assertArrayEquals(productFibo.productFibo(1), expectedResult);
+    }
+
+    @Test
+    public void test_Product_Of_Fibonacci_714(){
+        expectedResult = new int[]{21, 34, 1};
+        assertArrayEquals(productFibo.productFibo(714), expectedResult);
     }
 }
