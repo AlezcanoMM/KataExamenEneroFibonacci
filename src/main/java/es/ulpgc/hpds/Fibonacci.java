190cc83f4a207b6aa450f439578bbaf64ee694aa
Alezcanomm <Alezcanomm@gmail.com>
2022-01-21 10:11:19 +0000

    Refactores class Fibonaccitest and Fibonacci

--- src/main/java/es/ulpgc/hpds/Fibonacci.java
+++ src/main/java/es/ulpgc/hpds/Fibonacci.java
@@ -3,7 +3,7 @@ package es.ulpgc.hpds;
 public class Fibonacci {
 
     public int getFibonacciOf(int n) {
-        if(n == 0 || n == 1){
+        if(n <= 1){
             return n;
         }
         return getFibonacciOf(n-1) + getFibonacciOf(n-2);
--- src/test/java/es/ulpgc/hpds/FibonacciTest.java
+++ src/test/java/es/ulpgc/hpds/FibonacciTest.java
@@ -3,31 +3,35 @@ package es.ulpgc.hpds;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FibonacciTest {
 
+    private Fibonacci fibo;
+
+    @Before
+    public void setUp(){
+        fibo = new Fibonacci();
+    }
+
     @Test
     public void test_Fibonacci_of_0(){
-        Fibonacci fibo = new Fibonacci();
         assertEquals(fibo.getFibonacciOf(0), 0);
     }
 
     @Test
     public void test_Fibonacci_of_1(){
-        Fibonacci fibo = new Fibonacci();
         assertEquals(fibo.getFibonacciOf(1), 1);
     }
 
     @Test
     public void test_Fibonacci_of_2(){
-        Fibonacci fibo = new Fibonacci();
         assertEquals(fibo.getFibonacciOf(2), 1);
     }
 
     @Test
     public void test_Fibonacci_of_8(){
-        Fibonacci fibo = new Fibonacci();
         assertEquals(fibo.getFibonacciOf(8), 21);
     }
 }
